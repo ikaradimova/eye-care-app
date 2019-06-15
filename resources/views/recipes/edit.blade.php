03d77844bf4267ae17a1612260a7ff708409b22e
ikaradimova <ikaradimova@gmail.com>
2019-06-15 15:52:52 +0300

    recipes completed

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -1,53 +1,39 @@
 @extends("layouts.app")
 @section("content")
-
-    <section class="edit">
-        <h2>Редактиране на рецепта <span> {{$recipe->title}} </span></h2>
-        {!! Form::open(['action' => ['RecipesController@update', $recipe->id ], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-        <div class="form-group-custom">
-            {{Form::label('title','Заглавие')}}
-            {{Form::text('title',  $recipe->title ,['placeholder' => 'This is the recipe name' ])}}
-        </div>
-        <div class="form-group-custom">
-            {{Form::label('preptime','Време за подготовка')}}
-            {{Form::text('preptime', $recipe->preptime ,['placeholder' => 'The time it would take for the whole recipe to be completed, doesnt have to be exact' ])}}
-        </div>
-        <div class="form-group-custom">
-            {{Form::label('image','Изображение')}}
-            {{Form::file('image')}}
-            {{Form::hidden("imagename", $recipe->cover)}}
-        </div>
-        <div class="form-group-custom">
-            {{Form::label('description','Описание')}}
-            {{Form::textarea('description', $recipe->description ,['placeholder' => 'Describe the recipe, give a little overview of what to expect ' ])}}
-        </div>
-        <div class="form-group-custom">
-            {{Form::label('ingredients','Съставки')}}
-            {{Form::textarea('ingredients', $recipe->ingredients ,['placeholder' => 'The ingredients needed for the recipe, separate them with commas' ])}}
-        </div>
-        <div class="form-group-custom">
-            {{Form::label('preparation','Приготвяне')}}
-            {{Form::textarea('preparation', $recipe->preparation,['placeholder' => 'The detailed recipe preparation process' ])}}
-        </div>
-        <div class="form-group2">
-            <div class="form-edit-meta">
-                <button type="submit">Добавяне</button>
+    <main role="main">
+        <section class="edit">
+            <h2>Редактиране на: <span> {{$recipe->title}} </span></h2>
+            {!! Form::open(['action' => ['RecipesController@update', $recipe->id ], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+            <div class="form-group-custom col">
+                {{Form::label('title','Заглавие')}}
+                {{Form::text('title',  $recipe->title ,['placeholder' => 'This is the recipe name' ])}}
             </div>
-        </div>
-        {{Form::hidden("_method", "PUT")}}
-
-        {!! Form::close() !!}
-
-
-    </section>
-    {{--    @if (!Auth::guest())--}}
-    {{--        @if (Auth::user()->id == $post->user_id)--}}
-    {!! Form::open(["action" => ["RecipesController@destroy", $recipe->id], "method" => "POST", "class" => "delete"]) !!}
-    {{Form::hidden("_method","DELETE")}}
-    {{--            {{Form::submit('Delete this recipe', '')}}--}}
-    <button type="submit">Изтриване на рецепта</button>
-    {!! Form::close() !!}
-    {{--        @endif--}}
-    {{--    @endif--}}
-
+            <div class="form-group-custom col">
+                {{Form::label('preptime','Време за подготовка')}}
+                {{Form::text('preptime', $recipe->preptime ,['placeholder' => 'The time it would take for the whole recipe to be completed, doesnt have to be exact' ])}}
+            </div>
+            <div class="form-group-custom col">
+                {{Form::label('image','Изображение')}}
+                {{Form::file('image')}}
+                {{Form::hidden("imagename", $recipe->cover)}}
+            </div>
+            <div class="form-group-custom col">
+                {{Form::label('description','Описание')}}
+                {{Form::textarea('description', $recipe->description ,['placeholder' => 'Describe the recipe, give a little overview of what to expect ' ])}}
+            </div>
+            <div class="form-group-custom col">
+                {{Form::label('ingredients','Съставки')}}
+                {{Form::textarea('ingredients', $recipe->ingredients ,['placeholder' => 'The ingredients needed for the recipe, separate them with commas' ])}}
+            </div>
+            <div class="form-group-custom col">
+                {{Form::label('preparation','Приготвяне')}}
+                {{Form::textarea('preparation', $recipe->preparation,['placeholder' => 'The detailed recipe preparation process' ])}}
+            </div>
+            <div class="form-group2 col" style="margin-top: 1em;">
+                <div class="form-edit-meta">
+                    <button type="submit" class="btn btn-primary" style="background: #1eb6a7">Редакция</button>
+                </div>
+            </div>
+        </section>
+    </main>
 @endsection
--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,36 +1,61 @@
 @extends("layouts.app")
 @section("content")
-    <section>
-        <h2>{{$recipe->title}}  <span>готово за {{ $recipe->preptime}} </span></h2>
-        <div class="recipeContainer">
-            <div>
-                <img class="cover" src="../storage/covers/{{$recipe->cover}}">
-            </div>
-
-            <div>
-                <p class="title">Съставки</p>
-                <p>{{$recipe->ingredients}}</p>
-            </div>
-            <div>
-                <p class="title">Описание</p>
-                <p>{{$recipe->description}}</p>
-            </div>
-            <div>
-                <p class="title">Приготвяне</p>
-                <p>{{$recipe->preparation}}</p>
-            </div>
-            <div>
-{{--                <p>Recipe by: <strong>{{$recipe->user->name}}</strong></p>--}}
-            </div>
-
-{{--            @if (!Auth::guest())--}}
-{{--                @if (Auth::user()->id == $post->user_id)--}}
+    <?php $role = 'guest'; ?>
+    @if (Auth::check())
+        <?php
+        //        use Illuminate\Support\Facades\DB;
+        $user = auth()->user();
+        $roleId = DB::table('role_user')->where('user_id', $user->id)->first()->role_id;
+        $role = strtolower(DB::table('roles')->where('id', $roleId)->first()->name);
+        ?>
+    @endif
+    @if ($role === 'admin')
+        <main role="main">
+            @endif
+            <section>
+                <h2>{{$recipe->title}} <span>готово за {{ $recipe->preptime}} </span></h2>
+                <div class="recipeContainer">
                     <div>
-                        <p><a class="editPost" href="/recipes/{{$recipe->id}}/edit">Редактиране на рецепта</a></p>
+                        <img class="cover" src="../storage/covers/{{$recipe->cover}}">
                     </div>
-{{--                @endif--}}
-{{--            @endif--}}
 
-        </div>
-    </section>
+                    <div>
+                        <p class="title">Съставки</p>
+                        <p>{{$recipe->ingredients}}</p>
+                    </div>
+                    <div>
+                        <p class="title">Описание</p>
+                        <p>{{$recipe->description}}</p>
+                    </div>
+                    <div>
+                        <p class="title">Приготвяне</p>
+                        <p>{{$recipe->preparation}}</p>
+                    </div>
+                    @if ($role === 'admin')
+                        <div style="margin-top: 10px">
+                                {!! Form::open(["action" => ["RecipesController@edit", $recipe->id], "method" => "POST", "class" => "edit"]) !!}
+                                {{Form::hidden("_method","GET")}}
+                                <button type="submit" class="btn btn-primary float-left"
+                                        style="background: #1eb6a7;
+                                        margin-left: 100px;">
+                                    Редактиране на рецепта
+                                </button>
+                                {!! Form::close() !!}
+                                {{--                            <button type="submit"><a class="editPost" href="/recipes/{{$recipe->id}}/edit">Редактиране на рецепта</a></button>--}}
+                                {!! Form::open(["action" => ["RecipesController@destroy", $recipe->id], "method" => "POST", "class" => "delete"]) !!}
+                                {{Form::hidden("_method","DELETE")}}
+                                <button type="submit"
+                                        class="btn btn-primary float-right"
+                                        style="background: red; margin-right: 100px;">
+                                    Изтриване на рецепта
+                                </button>
+                                {!! Form::close() !!}
+                        </div>
+                    @endif
+
+                </div>
+            </section>
+            @if (strtolower($role) === 'admin')
+        </main>
+    @endif
 @endsection
